acb10a46d9689c5abf000b397431a8bce01c5fc8
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-13 23:06:00 -0800

    fixes and renames rootFinder

--- math/rootFinder.py
+++ math/rootFinder.py
@@ -1,3 +1,6 @@
+# FIRST: Takes in a user-input value and returns IF a number is prime
+# IF NOT PRIME: returns all ROOTs
+
 x = input("What number would you like to find all roots?")
 
 rootArr = []
@@ -5,7 +8,7 @@ rootArr = []
 def findRoot(x):
     for i in range(int(x)):
         if (int(x) % (int(x) - i)) == 0:
-            rootArr.append(i + 1)
+            rootArr.append(int(x) - i)
     if len(rootArr) > 2:
         print(x, "is NOT a prime")
         print("divisors", rootArr)
