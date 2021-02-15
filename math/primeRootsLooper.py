7e5572541622ad947b82d4d63889e9cf5ece4f31
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-14 21:46:06 -0800

    new math prime looper

---
+++ math/primeRootsLooper.py
@@ -0,0 +1,37 @@
+x = input("What number?")
+
+
+rootArr = []
+primeArr = []
+
+def getMod(x, y):
+    return x % y
+
+def getRoot(num):
+    curNum = int(num)
+    for i in range(curNum):
+        if i != 0 and getMod(curNum, i + 1) == 0:
+            rootArr.append(i + 1)
+            curNum = int(curNum / (i + 1))
+            getRoot(curNum)
+            break
+
+def allRoots(x):
+    getRoot(x)
+    if len(rootArr) > 1:
+        print("All roots of", x, ":", rootArr)
+        rootArr.clear()
+    else:
+        print(x, "is a PRIME number!")
+        primeArr.append(x)
+
+
+def primeLooper(val):
+    j = int(val)
+    for i in range(j):
+        allRoots(j - i)
+    print("All primes from", j, "to 1:",primeArr)
+        
+
+primeLooper(x)
+
